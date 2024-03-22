11b4a499ba165679b335f0fde6cb37158cc2bbff
Pankaj <pankaj.kumar@volunteer.heartfulness.org>
2024-03-22 14:15:13 +0530

    dons QR code scanner page UI

--- lib/screens/qr_code_scanner.dart
+++ lib/screens/qr_code_scanner.dart
@@ -15,22 +15,49 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MobileScanner(
-        // fit: BoxFit.contain,
-        onDetect: (capture) {
-          final List<Barcode> barcodes = capture.barcodes;
-           var plantId = barcodes[0].rawValue;
-           if(plantId!=null){
-             try {
-               int number = int.parse(plantId);
-               Navigator.pop(context);
-               Navigator.of(context).push(MaterialPageRoute(builder:(context)=>PlantScreen(plantId: plantId)));
-               print('Parsed successfully: $number');
-             } catch (e) {
-               print('Error: $e');
-             }
-           }
-          }
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        iconTheme: IconThemeData(
+          color: Colors.white
+
+        ),
+        title: const Text('QR/Barcode Scanner', style: TextStyle(color: Colors.white),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 100,),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text('Scan Heartyculture Nursery Plant QR/Barcode ' , style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
+            ),
+            const SizedBox(height: 30,),
+
+
+            Container(
+              height: 300,
+              child: MobileScanner(
+                // fit: BoxFit.contain,
+                onDetect: (capture) {
+                  final List<Barcode> barcodes = capture.barcodes;
+                   var plantId = barcodes[0].rawValue;
+                   if(plantId!=null){
+                     try {
+                       int number = int.parse(plantId);
+                       Navigator.pop(context);
+                       Navigator.of(context).push(MaterialPageRoute(builder:(context)=>PlantScreen(plantId: plantId)));
+                       print('Parsed successfully: $number');
+                     } catch (e) {
+                       print('Error: $e');
+                     }
+                   }
+                  }
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
